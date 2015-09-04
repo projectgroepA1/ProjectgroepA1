2675c7efe6be7b8f72a11dd6f5ddf486d7db30a2
Supermaniac101 <Timschijvenaars@hotmail.com>
2015-09-04 11:49:39 +0200

    Reset added

--- FietsApp/Communication.cs
+++ FietsApp/Communication.cs
@@ -10,32 +10,38 @@ namespace FietsApp
 {
     class Communication
     {
-        private SerialPort serial;
+        private SerialPort mySerialPort;
         public Communication(string port)
         {
-            SerialPort mySerialPort = new SerialPort(port);
+            mySerialPort = new SerialPort(port);
             mySerialPort.BaudRate = 9600;
             mySerialPort.Parity = Parity.None;
             mySerialPort.StopBits = StopBits.One;
             mySerialPort.DataBits = 8;
             mySerialPort.Handshake = Handshake.None;
 
-            mySerialPort.Open();
+            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
 
+            mySerialPort.Open();
+            sendCommand();
             Console.WriteLine("Press any key to continue...");
             Console.WriteLine();
             Console.ReadKey();
             mySerialPort.Close();
         }
 
+        public void sendCommand()
+        {
+            mySerialPort.WriteLine("RS");
+        }
         
 
-        private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        public static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
             SerialPort sp = (SerialPort)sender;
             string indata = sp.ReadExisting();
-            Debug.Print("Data Received:");
-            Debug.Print(indata);
+            Console.WriteLine("Data Received:");
+            Console.WriteLine(indata);
         }
 
 
