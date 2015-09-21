e18f0677ddabebdfb8a7660d6597f9632c8c5a29
Remco <Remco.boys@gmail.com>
2015-09-21 12:25:34 +0200

    server setup en clienthandler added

---
+++ ServerApp/ClientHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace ServerApp
+{
+    class ClientHandler
+    {
+        private TcpClient tc;
+        private Server server;
+
+        public ClientHandler(TcpClient tc,Server server)
+        {
+            this.tc = tc;
+            this.server = server;
+        }
+
+        public void ClientSetUp(Object b)
+        {
+            
+        }
+    }
+}
--- ServerApp/Server.cs
+++ ServerApp/Server.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Net;
 using System.Net.Sockets;
 
@@ -10,13 +11,40 @@ namespace ServerApp
 {
     class Server
     {
+        public IPAddress IP { get; set; }
+        public static readonly int Port = 1967;
+        private TcpListener serverListener;
+        private List<Tuple<ClientHandler, Thread>> clients;
+
         public Server()
         {
-            Console.WriteLine(Dns.GetHostName());
-            IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
-            foreach (IPAddress ip in localIPs)
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            IP = ipHostInfo.AddressList[0];
+            clients = new List<Tuple<ClientHandler, Thread>>(); 
+            Console.WriteLine("Server IP-address: {0}",IP.ToString());
+            serverListener = new TcpListener(IP,Port);
+        }
+
+        public void ServerSetUp(Object ob)
+        {
+            serverListener.Start();
+            while(true)
             {
-                Console.WriteLine(ip);
+                Thread.Sleep(1000);
+                TcpClient tc = new TcpClient(IP.ToString(), Port);
+                for (int i = 5; i > 0; i--)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(1000);
+                }
+                
+
+
+                TcpClient tcTest = serverListener.AcceptTcpClient();
+                ClientHandler clientHandler = new ClientHandler(tcTest,this);
+                Thread clientThread = new Thread(clientHandler.ClientSetUp);
+                clients.Add(new Tuple<ClientHandler,Thread>(clientHandler,clientThread));
+                clientThread.Start();                
             }
         }
     }
