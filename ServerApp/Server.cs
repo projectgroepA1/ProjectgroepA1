34f616b82d466aefbcc4313f585a8b9823a07713
Remco <Remco.boys@gmail.com>
2015-09-21 10:34:57 +0200

    ServerTest

---
+++ ServerApp/Server.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerApp
+{
+    class Server
+    {
+        public Server()
+        {
+            Console.WriteLine(Dns.GetHostName());
+            IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress ip in localIPs)
+            {
+                Console.WriteLine(ip);
+            }
+        }
+    }
+}
