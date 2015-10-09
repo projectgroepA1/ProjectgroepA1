cb4c26fed1e05928ec9d03b177577588c3517187
Gilian Joosen <gilian_joosen@hotmail.com>
2015-10-09 17:50:52 +0200

    -bicycle commands packet

---
+++ NetLib/PacketBicycleCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLib
+{
+    class PacketBicycleCommand:Packet
+    {
+
+        string command { get; }
+
+        public PacketBicycleCommand(string command)
+        {
+            this.command = command;
+        }
+
+        public override void handleServerSide(ServerInterface serverInterface)
+        {
+
+        }
+
+        public override void handleClientSide(ClientInterface clientInterface)
+        {
+
+        }
+    }
+}
